03d1919f3718ae12730ce39049912cd8005c0005
Priit Ulmas <priit.ulmas@gmail.com>
2018-01-19 16:04:23 +0200

    add dataset description

--- L1_apples_and_oranges.py
+++ L1_apples_and_oranges.py
@@ -3,8 +3,16 @@
 Created on Fri Jan 19 15:45:48 2018
 
 @author: ulmas
-"""
 
+Learning dataset
+WEIGHT  TEXTURE LABEL
+140     smooth  apple
+130     smooth  apple
+150     bumpy   orange
+170     bumpy   orange
+
+
+"""
 from sklearn import tree
 features = [[140,1],[130,1],[150,0],[170,0]]
 labels = [0,0,1,1]
