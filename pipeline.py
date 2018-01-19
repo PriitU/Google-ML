13a1665c7e9fff76d0252c87bcf00b99f089c052
Priit Ulmas <priit.ulmas@gmail.com>
2018-01-19 17:49:49 +0200

    L4 pipeline

---
+++ pipeline.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 17:34:28 2018
+
+@author: ulmas
+"""
+
+from sklearn import datasets
+iris = datasets.load_iris()
+
+x = iris.data
+y = iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x,y,test_size=.5)
+
+"""
+#Decision tree classifier
+from sklearn import tree
+my_classifier = tree.DecisionTreeClassifier()
+"""
+
+"""
+#K-nearest neighbors classifier
+"""
+from sklearn.neighbors import KNeighborsClassifier
+my_classifier = KNeighborsClassifier()
+
+my_classifier.fit(X_train, y_train)
+
+predictions = my_classifier.predict(X_test)
+#print(predictions)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, predictions))
