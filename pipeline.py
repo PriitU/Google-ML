cd704fb8abde666fd1f7c41b7912a4b465f061e7
Priit Ulmas <priit.ulmas@gmail.com>
2018-01-19 17:53:52 +0200

    L4 pipeline

--- pipeline.py
+++ pipeline.py
@@ -4,13 +4,14 @@ Created on Fri Jan 19 17:34:28 2018
 
 @author: ulmas
 """
-
+#import dataset
 from sklearn import datasets
 iris = datasets.load_iris()
 
 x = iris.data
 y = iris.target
 
+#split dataset to train and test
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(x,y,test_size=.5)
 
@@ -26,8 +27,10 @@ my_classifier = tree.DecisionTreeClassifier()
 from sklearn.neighbors import KNeighborsClassifier
 my_classifier = KNeighborsClassifier()
 
+#train the classifier
 my_classifier.fit(X_train, y_train)
 
+#test the classifier
 predictions = my_classifier.predict(X_test)
 #print(predictions)
 
